17fc9e36b2bc9e1dd4264cf3a9b728fcbc434943
Guy Chriqui <guy@Guys-MacBook-Air.local>
2019-12-13 01:33:16 +0200

    Add nlp fix

--- Services/Domain/nlp_diagnosis.js
+++ Services/Domain/nlp_diagnosis.js
@@ -5,12 +5,13 @@ const CONFIDENCE_LEVEL = 0.5;
 exports.analyzeNlp = function getNlpResults(message) {
 	if (message && message.message && message.message.nlp) {
 		const requiredNLPResults = message.message.nlp;
+		console.log(requiredNLPResults);
 		filterUnConfident(requiredNLPResults, CONFIDENCE_LEVEL);
 		return requiredNLPResults;
 	}
 };
 
-function filterUnconfident(nlpEntitiesArray, limit) {
+function filterUnConfident(nlpEntitiesArray, limit) {
 	if (Array.isArray(nlpEntitiesArray) && nlpEntitiesArray.length > 0) {
 		nlpEntitiesArray.filter((nlpEntity) => {
 			return nlpEntity.confidence >= limit;
