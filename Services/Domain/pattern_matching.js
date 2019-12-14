a89f527cad1ee237d7054d7d496a8e643e22f822
Guy Chriqui <guy@Guys-MacBook-Air.local>
2019-12-14 22:26:04 +0200

    attempt to fix response undefine

--- Services/Domain/pattern_matching.js
+++ Services/Domain/pattern_matching.js
@@ -5,6 +5,7 @@ const stringUtils = require('../../Utils/string_ultis');
 
 exports.matchPattern = function(message) {
 	if (message === 'GET_STARTED') {
+		console.log('its get started pattern');
 		return require('../../Patterns/greeting_pattern');
 	}
 };
