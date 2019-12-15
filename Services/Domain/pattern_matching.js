e7c2376265047b6197bc3c19f5841f5f4db2153e
Guy Chriqui <Guy.Chriqui@osrenterprises.com>
2019-12-15 14:29:04 +0200

    Update nlp

--- Services/Domain/pattern_matching.js
+++ Services/Domain/pattern_matching.js
@@ -6,17 +6,17 @@ const nlpDiagnosis = require("./nlp_diagnosis");
 // Find best matching pattern to  incoming message
 exports.matchPattern = function(message) {
   // Get intent of message
-  const diagnosis = nlpDiagnosis.analyzeNlp(message);
+  const diagnosis = nlpDiagnosis.getNlpResults(message);
   const diagnosisString = JSON.stringify(diagnosis, null, 4);
   console.log("Message diagnosis:" + diagnosisString);
 
-  switch (key) {
-    case value:
-      break;
+  //   switch (key) {
+  //     case value:
+  //       break;
 
-    default:
-      break;
-  }
+  //     default:
+  //       break;
+  //   }
 
   if (message === "GET_STARTED") {
     return require("../../Patterns/greeting_pattern");
