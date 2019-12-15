6f70d96945b9b4a941ac43cd3db354d0be79ef20
Guy Chriqui <Guy.Chriqui@osrenterprises.com>
2019-12-15 17:06:28 +0200

    Get intent fix

--- Services/Domain/nlp_diagnosis.js
+++ Services/Domain/nlp_diagnosis.js
@@ -22,10 +22,10 @@ function filterUnConfident(nlpEntitiesArray, limit) {
 
 // Return entity data by a given name
 exports.getEntity = function getNlpEntityByName(message, entity) {
-  return message && message.nlp && message.nlp.entities && message.nlp.entities[entity][0];
+  return message && message.nlp && message.nlp.entities && message.nlp.entities[entity];
 };
 
 // Return message intent
 exports.getIntent = function getIntent(message) {
-  return message && message.nlp && message.nlp.entities && message.nlp.entities["intent"][0];
+  return message && message.nlp && message.nlp.entities && message.nlp.entities["intent"];
 };
