247d02a6011613caa777996e7d315af3ca4e5ae6
Guy Chriqui <Guy.Chriqui@osrenterprises.com>
2019-12-15 15:06:27 +0200

    Update nlp

--- Utils/MessageUtils/sender_actions.js
+++ Utils/MessageUtils/sender_actions.js
@@ -1,26 +1,24 @@
-const wait = require('wait-for-stuff');
-const incomingMessageUtils = require('../MessageUtils/incoming_message_ultis');
-const facebook = require('fb-messenger-bot-api');
-
+const wait = require("wait-for-stuff");
+const facebook = require("fb-messenger-bot-api");
 const messageClient = new facebook.FacebookMessagingAPIClient(process.env.PAGE_ACCESS_TOKEN);
 
 // Mark typing and wait 'x' amount of time before disable typing (writing time)
 exports.markTyping = function(senderId, message) {
-	messageClient.toggleTyping(senderId, true);
-	wait.for.time(calculateWaitTime(message));
-	messageClient.toggleTyping(senderId, false);
+  messageClient.toggleTyping(senderId, true);
+  wait.for.time(calculateWaitTime(message));
+  messageClient.toggleTyping(senderId, false);
 };
 
 // Wait 'x' amount of time (reading time) and then mark message as seen
 exports.markSeen = function(senderId, incomingMeassage) {
-	messageClient.markSeen(senderId);
-	const incomingMeassageString = incomingMessageUtils.getTextFromMessage(incomingMeassage);
-	wait.for.time(calculateWaitTime(incomingMeassageString));
+  messageClient.markSeen(senderId);
+  const incomingMeassageString = incomingMeassage.message.text;
+  wait.for.time(calculateWaitTime(incomingMeassageString));
 };
 
 // Calaculate amount of time by text length
 function calculateWaitTime(message) {
-	let delay = message.length / 10;
-	if (delay > 7) delay = 7;
-	return delay;
+  let delay = message.length / 10;
+  if (delay > 7) delay = 7;
+  return delay;
 }
