d3e95ed590146b6568a9082c27a501f72e1ecb00
Guy Chriqui <guy@Guys-MacBook-Air.local>
2019-12-16 12:18:03 +0200

    Typo fix

--- Services/Domain/nlp_diagnosis.js
+++ Services/Domain/nlp_diagnosis.js
@@ -1,37 +1,37 @@
-"use strict";
+'use strict';
 
 const CONFIDENCE_LEVEL = 0.5;
 
 // Returns the entitis found from nlp model in JSON format
 exports.getNlpResults = function getNlpResults(message) {
-  if (message && message.message && message.message.nlp) {
-    const requiredNLPResults = message.message.nlp;
-    filterUnConfident(requiredNLPResults, CONFIDENCE_LEVEL);
-    return requiredNLPResults;
-  }
+	if (message && message.message && message.message.nlp) {
+		const requiredNLPResults = message.message.nlp;
+		filterUnConfident(requiredNLPResults, CONFIDENCE_LEVEL);
+		return requiredNLPResults;
+	}
 };
 
 // Remove entitis with less then 'limit' confidence
 function filterUnConfident(nlpEntitiesArray, limit) {
-  if (Array.isArray(nlpEntitiesArray) && nlpEntitiesArray.length > 0) {
-    nlpEntitiesArray.filter(nlpEntity => {
-      return nlpEntity.confidence >= limit;
-    });
-  }
+	if (Array.isArray(nlpEntitiesArray) && nlpEntitiesArray.length > 0) {
+		nlpEntitiesArray.filter((nlpEntity) => {
+			return nlpEntity.confidence >= limit;
+		});
+	}
 }
 
 // Return entity data by a given name
 exports.getEntity = function getNlpEntityByName(message, entity) {
-  if (message && message.message && message.message.nlp) {
-    const requiredNLPResults = message.message.nlp;
-    return requiredNLPResults.entities[entity][0];
-  }
+	if (message && message.message && message.message.nlp) {
+		const requiredNLPResults = message.message.nlp;
+		return requiredNLPResults.entities[entity][0];
+	}
 };
 
 // Return message intent
 exports.getIntent = function getIntent(message) {
-  if (message && message.message && message.message.nlp) {
-    const requiredNLPResults = message.message.nlp;
-    return requiredNLPResults.entities["intent"][0];
-  }
+	if (message && message.message && message.message.nlp) {
+		const requiredNLPResults = message.message.nlp;
+		return requiredNLPResults.entities['intent'][0];
+	}
 };
