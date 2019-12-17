311e8e6432c157289a31fe49556baeeb9ad607cb
Guy Chriqui <Guychriquii@gmail.com>
2019-12-17 21:50:11 +0200

    בישמעק גקךשט אןצקד

--- Utils/MessageUtils/sender_actions.js
+++ Utils/MessageUtils/sender_actions.js
@@ -15,12 +15,12 @@ exports.markTyping = function(senderId, message) {
 exports.markSeen = function(senderId, incomingMeassage) {
 	messageClient.markSeen(senderId);
 	const incomingMeassageString = incomingMessageUtils.getTextFromMessage(incomingMeassage);
-	wait.for.time(calculateWaitTime(incomingMeassageString));
+	wait.for.time(calculateWaitTime(incomingMeassageString) - 1);
 };
 
 // Calaculate amount of time by text length
 function calculateWaitTime(message) {
 	let delay = message.length / 10;
-	if (delay > 5) delay = 5;
+	if (delay > 4) delay = 4;
 	return delay;
 }
