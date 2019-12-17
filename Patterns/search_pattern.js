d764772791800ee6419016e7a5da56baffad4dec
Guy Chriqui <Guy.Chriqui@osrenterprises.com>
2019-12-17 15:37:01 +0200

    Add search query response

--- Patterns/search_pattern.js
+++ Patterns/search_pattern.js
@@ -3,10 +3,10 @@ const nlpDiagnosis = require("../Services/Domain/nlp_diagnosis");
 
 // Get the best matching response to the message
 exports.getResponse = function(messageObject) {
-  console.log("object : ", nlpDiagnosis.getEntity(messageObject, "object"));
-  console.log("amount_of_money : ", nlpDiagnosis.getEntity(messageObject, "amount_of_money"));
-  console.log("pick_up : ", nlpDiagnosis.getEntity(messageObject, "pick_up"));
-  console.log("location : ", nlpDiagnosis.getEntity(messageObject, "location"));
+  //   console.log("object : ", nlpDiagnosis.getEntity(messageObject, "object"));
+  //   console.log("amount_of_money : ", nlpDiagnosis.getEntity(messageObject, "amount_of_money"));
+  //   console.log("pick_up : ", nlpDiagnosis.getEntity(messageObject, "pick_up"));
+  //   console.log("location : ", nlpDiagnosis.getEntity(messageObject, "location"));
 
   const response = "אני על זה!";
 
