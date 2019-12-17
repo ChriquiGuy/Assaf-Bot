ade00813bd344fa757a4d13b1afac229f2c411b1
Guy Chriqui <Guy.Chriqui@osrenterprises.com>
2019-12-17 12:02:31 +0200

    Add search query response

--- Patterns/search_pattern.js
+++ Patterns/search_pattern.js
@@ -3,27 +3,26 @@ const nlpDiagnosis = require("../Services/Domain/nlp_diagnosis");
 
 // Get the best matching response to the message
 exports.getResponse = function(messageObject) {
+  console.log("object :", nlpDiagnosis.getEntity(messageObject, "object"));
+  console.log("amount_of_money : ", nlpDiagnosis.getEntity(messageObject, "amount_of_money"));
+  console.log("pick_up : ", nlpDiagnosis.getEntity(messageObject, "pick_up"));
+  console.log("location : ", nlpDiagnosis.getEntity(messageObject, "location"));
 
-    console.log('object :' , nlpDiagnosis.getEntity(messageObject, "object"))
-    console.log('amount_of_money : ' , nlpDiagnosis.getEntity(messageObject, "amount_of_money"))
-    console.log('pick_up : ' , nlpDiagnosis.getEntity(messageObject, "pick_up"))
-    console.log('location : ' , nlpDiagnosis.getEntity(messageObject, "location"))
+  const response = "אני על זה!";
 
-    const response = "אני על זה!";
+  var description;
 
-    const description
+  const object = nlpDiagnosis.getEntity(messageObject, "object").value;
+  if (object) description += `מחפש לך ${object} `;
 
-    const object = nlpDiagnosis.getEntity(messageObject, "object").value;
-    if (object) description += `מחפש לך ${object} `
+  const object = nlpDiagnosis.getEntity(messageObject, "object");
 
-     const object = nlpDiagnosis.getEntity(messageObject, "object");
+  const responseActions = undefined;
 
-    const responseActions = undefined;
+  const response = {
+    messages: [response, description],
+    actions: [responseActions]
+  };
 
-    const response = {
-      messages: [response , description],
-      actions: [responseActions]
-    };
-
-     return response;
+  return response;
 };
