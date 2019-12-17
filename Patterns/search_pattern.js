cf406529a468117a2e98d0550a52d90c7605d128
Guy Chriqui <Guy.Chriqui@osrenterprises.com>
2019-12-17 11:45:06 +0200

    Add search query response

---
+++ Patterns/search_pattern.js
@@ -0,0 +1,29 @@
+"use strict";
+const nlpDiagnosis = require("../Services/Domain/nlp_diagnosis");
+
+// Get the best matching response to the message
+exports.getResponse = function(messageObject) {
+
+    console.log('object :' , nlpDiagnosis.getEntity(messageObject, "object"))
+    console.log('amount_of_money : ' , nlpDiagnosis.getEntity(messageObject, "amount_of_money"))
+    console.log('pick_up : ' , nlpDiagnosis.getEntity(messageObject, "pick_up"))
+    console.log('location : ' , nlpDiagnosis.getEntity(messageObject, "location"))
+
+  const response = "אני על זה!";
+
+  const description
+
+  const object = nlpDiagnosis.getEntity(messageObject, "object").value;
+  if (object) description += `מחפש לך ${object} `
+
+  const object = nlpDiagnosis.getEntity(messageObject, "object");
+
+  const responseActions = undefined;
+
+  const response = {
+    messages: [response , description],
+    actions: [responseActions]
+  };
+
+  return response;
+};
--- Services/Domain/nlp_diagnosis.js
+++ Services/Domain/nlp_diagnosis.js
@@ -1,41 +1,43 @@
-'use strict';
+"use strict";
 
 const CONFIDENCE_LEVEL = 0.5;
 
 // Returns the entitis found from nlp model in JSON format
 exports.getNlpResults = function getNlpResults(messageObject) {
-	if (messageObject && messageObject.message && messageObject.message.nlp) {
-		const requiredNLPResults = messageObject.message.nlp;
-		filterUnConfident(requiredNLPResults, CONFIDENCE_LEVEL);
-		return requiredNLPResults;
-	}
+  if (messageObject && messageObject.message && messageObject.message.nlp) {
+    const requiredNLPResults = messageObject.message.nlp;
+    filterUnConfident(requiredNLPResults, CONFIDENCE_LEVEL);
+    return requiredNLPResults;
+  }
 };
 
 // Remove entitis with less then 'limit' confidence
 function filterUnConfident(nlpEntitiesArray, limit) {
-	if (Array.isArray(nlpEntitiesArray) && nlpEntitiesArray.length > 0) {
-		nlpEntitiesArray.filter((nlpEntity) => {
-			return nlpEntity.confidence >= limit;
-		});
-	}
+  if (Array.isArray(nlpEntitiesArray) && nlpEntitiesArray.length > 0) {
+    nlpEntitiesArray.filter(nlpEntity => {
+      return nlpEntity.confidence >= limit;
+    });
+  }
 }
 
 // Return entity data by a given name
 exports.getEntity = function getNlpEntityByName(messageObject, entity) {
-	if (messageObject && messageObject.message && messageObject.message.nlp) {
-		const requiredNLPResults = messageObject.message.nlp;
-		return requiredNLPResults.entities[entity][0];
-	} else {
-		console.log('Entity ' + entity + ' not found');
-	}
+  if (messageObject && messageObject.message && messageObject.message.nlp) {
+    const requiredNLPResults = messageObject.message.nlp;
+    return requiredNLPResults.entities[entity][0];
+  } else {
+    console.log("Entity " + entity + " not found");
+    return undefined;
+  }
 };
 
 // Return message intent
 exports.getIntent = function getIntent(messageObject) {
-	if (messageObject && messageObject.message && messageObject.message.nlp) {
-		const nlp = messageObject.message.nlp;
-		return nlp.entities['intent'][0];
-	} else {
-		console.log('No intent found');
-	}
+  if (messageObject && messageObject.message && messageObject.message.nlp) {
+    const nlp = messageObject.message.nlp;
+    return nlp.entities["intent"][0];
+  } else {
+    console.log("No intent found");
+    return undefined;
+  }
 };
