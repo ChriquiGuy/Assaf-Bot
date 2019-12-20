65ac754522a5300bda7e76adda7619de4c336342
Guy Chriqui <Guychriquii@gmail.com>
2019-12-20 19:48:05 +0200

    add doorom options

--- Patterns/search_pattern.js
+++ Patterns/search_pattern.js
@@ -129,24 +129,20 @@ function unvalidObjectRespones() {
 			template_type: 'generic',
 			elements: [
 				{
-					title: 'Welcome!',
-					image_url: 'https://petersfancybrownhats.com/company_image.png',
-					subtitle: 'We have the right hat for everyone.',
+					title: 'Dooron - דורון חיפוש דירות',
+					image_url:
+						'https://scontent.ftlv5-1.fna.fbcdn.net/v/t1.0-9/49938853_348490665881339_2798763413499543552_n.png?_nc_cat=101&_nc_oc=AQkELNTKCDNF140YxnMLh-UIzqr_mJmUuFpuI7T2dIKZkXr3JWTe6lkBK77TAAaFpBg&_nc_ht=scontent.ftlv5-1.fna&oh=aff791450ba95a804e21f83eae3792e1&oe=5EB2ADD3',
+					subtitle: 'מוצא לך דירה בתל אביב',
 					default_action: {
 						type: 'web_url',
-						url: 'https://petersfancybrownhats.com/view?item=103',
+						url: 'https://www.facebook.com/dorontlv',
 						webview_height_ratio: 'tall'
 					},
 					buttons: [
 						{
 							type: 'web_url',
-							url: 'https://petersfancybrownhats.com',
-							title: 'View Website'
-						},
-						{
-							type: 'postback',
-							title: 'Start Chatting',
-							payload: 'DEVELOPER_DEFINED_PAYLOAD'
+							url: 'https://www.facebook.com/dorontlv',
+							title: 'שלח הודעה לדורון!'
 						}
 					]
 				}
