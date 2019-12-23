2d2cde4cccd91b46b226a0eac3c9716c3aa22143
Guy Chriqui <Guychriquii@gmail.com>
2019-12-23 14:39:14 +0200

    Update delay time of generic message

--- Utils/MessageUtils/sender_actions.js
+++ Utils/MessageUtils/sender_actions.js
@@ -5,9 +5,9 @@ const facebook = require('fb-messenger-bot-api');
 const messageClient = new facebook.FacebookMessagingAPIClient(process.env.PAGE_ACCESS_TOKEN);
 
 // Mark typing and wait 'x' amount of time before disable typing (writing time)
-exports.markTyping = function(senderId, message, time) {
+exports.markTyping = function(senderId, message, delayTime) {
 	messageClient.toggleTyping(senderId, true);
-	wait.for.time(calculateWaitTime(message, time));
+	wait.for.time(calculateWaitTime(message, delayTime));
 	messageClient.toggleTyping(senderId, false);
 };
 
@@ -19,17 +19,16 @@ exports.markSeen = function(senderId, incomingMeassage) {
 };
 
 // Calaculate amount of time by text length
-function calculateWaitTime(message, read = undefined, time) {
+function calculateWaitTime(message, read = undefined, delayTime) {
 	console.log('Time = ', time);
 	// If time exiest, return time
-	if (time) return time > 10 ? 10 : time;
+	if (delayTime) return delayTime > 10 ? 10 : delayTime;
 	// Get number of words in string
 	const wordCount = message.split(' ').length;
 	// Delay is 0.6(wirte)/0.3(read) sec for word
 	const wordTime = read ? 0.25 : 0.4;
 	// Calculate delay
 	var delay = wordCount * wordTime;
-	console.log('Delay = ', delay);
 	if (delay > 10) delay = 10;
 	return delay;
 }
