69d35f8eb3fb9e1f3258ed8c04fbf166f67cd925
Guy Chriqui <Guychriquii@gmail.com>
2020-01-23 14:09:30 +0200

    Crawler yad2 update

--- Crawlers/yad2_ crawler.js
+++ Crawlers/yad2_ crawler.js
@@ -1,42 +1,20 @@
-var Crawler = require('crawler');
+const cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
 var HTMLParser = require('node-html-parser');
 
-var c = new Crawler({
-	maxConnections: 10,
-	// This will be called for each crawled page
-	callback: function(error, res, done) {
-		if (error) {
-			console.log(error);
-		} else {
-			var $ = res.$;
-			// $ is Cheerio by default
-			//a lean implementation of core jQuery designed specifically for the server
-			console.log($('title').text());
-		}
-		done();
-	}
-});
+const url = 'https://www.yad2.co.il/products/all?info=%D7%90%D7%95%D7%A4%D7%A0%D7%99%D7%99%D7%9D';
 
-// Queue just one URL, with default callback
-c.queue('https://www.yad2.co.il/products/all?info=%D7%90%D7%95%D7%A4%D7%A0%D7%99%D7%99%D7%9D');
-
-// Queue URLs with custom callbacks & parameters
-c.queue([
-	{
-		uri: 'http://www.amazon.com',
-		jQuery: false,
-
-		// The global callback won't be called
-		callback: function(error, res, done) {
-			if (error) {
-				console.log(error);
-			} else {
-				const root = HTMLParser.parse(res.body);
-				// console.log('Root : ', root);
-				console.log('First Child : ', root.firstChild.querySelectorAll('#__nuxt').structure);
-				// console.log('query #feed_item: \n', root.querySelector('#__nuxt'));
-			}
-			done();
-		}
-	}
-]);
+puppeteer
+	.launch()
+	.then((browser) => browser.newPage())
+	.then((page) => {
+		return page.goto(url).then(function() {
+			return page.content();
+		});
+	})
+	.then((html) => {
+		var root = HTMLParser.parse(html);
+		const productList = root.querySelector('.feed_list');
+		console.log(productList);
+	})
+	.catch(console.error);
